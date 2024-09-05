ceb7e14a8a1d9ad4674d3757371709392a2f5ff9
Eyepan <iamteoggy@gmail.com>
2024-09-05 09:10:08 +0530

    feat: create info logger

--- src/logger/logger.go
+++ src/logger/logger.go
@@ -2,9 +2,14 @@ package logger
 
 import (
 	"fmt"
+	"log/slog"
 )
 
 func PrettyPrintStats(resolveCount int, totalResolveCount int, downloadCount int, totalDownloadCount int) {
 	// TODO: figure out a way to implement proper printing without inputting all the four numbers all the time
 	fmt.Printf("\r🔍[%d/%d] 📦[%d/%d]", resolveCount, totalResolveCount, downloadCount, totalDownloadCount)
+}
+
+func InfoLogger(message string) {
+	slog.Info(fmt.Sprintf("%s\n", message))
 }
