d5d290f413c347eaab6c3947571d5e265c733261
Eyepan <iamteoggy@gmail.com>
2024-09-11 07:44:53 +0530

    feat: fallsback to force if version resolution for cache hit is missed

--- src/metadata/metadata.go
+++ src/metadata/metadata.go
@@ -5,126 +5,135 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/Eyepan/yap/src/types"
 	"github.com/Eyepan/yap/src/utils"
 )
 
-func FetchMetadata(client *http.Client, pkg *types.Package, conf *types.YapConfig, forceFetchAndRefresh bool) (*types.Metadata, error) {
+func FetchMetadata(client *http.Client, pkg *types.Package, conf *types.YapConfig, forceFetch bool) (*types.Metadata, error) {
 	cacheDir, err := utils.GetCacheDir()
 	if err != nil {
 		return nil, fmt.Errorf("failed to get cache directory: %w", err)
 	}
 	cacheFile := filepath.Join(cacheDir, utils.SanitizePackageName(pkg.Name))
 
-	// this if should only happen if force is false
-	// Check if the cache file exists
-	if _, err := os.Stat(cacheFile); !forceFetchAndRefresh && err == nil {
-		// Cache file exists, read its contents
-		data, err := os.ReadFile(cacheFile)
-		if err != nil {
-			return nil, err
+	if !forceFetch {
+		if data, err := os.ReadFile(cacheFile); err == nil {
+			return utils.ReadMetadata(bytes.NewReader(data))
 		}
-
-		buf := bytes.NewReader(data)
-		return utils.ReadMetadata(buf)
 	}
 
-	// Cache file does not exist, fetch metadata from the server
-	registryURL := (*conf).Registry
-	authToken := (*conf).AuthToken
-	packageURL := fmt.Sprintf("%s/%s", registryURL, pkg.Name)
-
-	req, err := http.NewRequest("GET", packageURL, nil)
+	registryURL := fmt.Sprintf("%s/%s", conf.Registry, pkg.Name)
+	req, err := http.NewRequest("GET", registryURL, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	// Add the auth token to the request headers
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", authToken))
-	req.Header.Add("Accept", "application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*") // compressed registry data, for faster metadata resolutions
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", conf.AuthToken))
+	req.Header.Add("Accept", "application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*")
 
-	// Send the request
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
-	// Read the response body
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	// Unmarshal the response body into a types.Metadata variable
 	var metadata types.Metadata
-	err = json.Unmarshal(body, &metadata)
-	if err != nil {
+	if err := json.Unmarshal(body, &metadata); err != nil {
 		return nil, err
 	}
 
-	// Ensure the cache directory exists
 	if err := os.MkdirAll(cacheDir, os.ModePerm); err != nil {
 		return nil, err
 	}
 
-	// Write the metadata to the cache file in binary format
 	var buf bytes.Buffer
 	if err := utils.WriteMetadata(&buf, metadata); err != nil {
 		return nil, err
 	}
 
-	file, err := os.Create(cacheFile)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	if _, err := file.Write(buf.Bytes()); err != nil {
+	if err := os.WriteFile(cacheFile, buf.Bytes(), os.ModePerm); err != nil {
 		return nil, err
 	}
 
 	return &metadata, nil
 }
 
-func FetchVersionMetadata(client *http.Client, pkg *types.Package, npmrc *types.YapConfig, forceFetchAndRefresh bool) (types.VersionMetadata, error) {
-	md, err := FetchMetadata(client, pkg, npmrc, forceFetchAndRefresh)
+func FetchVersionMetadata(client *http.Client, pkg *types.Package, conf *types.YapConfig, forceFetch bool) (types.VersionMetadata, error) {
+	metadata, err := FetchMetadata(client, pkg, conf, forceFetch)
 	if err != nil {
 		return types.VersionMetadata{}, fmt.Errorf("failed to fetch metadata for package %s@%s: %w", pkg.Name, pkg.Version, err)
 	}
-	versionsList := make([]string, len(md.Versions))
-	i := 0
-	for k := range md.Versions {
-		versionsList[i] = k
-		i++
+
+	resolvedVersion := resolveVersion(pkg, metadata)
+	if err := checkResolvedVersion(resolvedVersion); err != nil {
+		metadata, err = FetchMetadata(client, pkg, conf, true)
+		if err != nil {
+			return types.VersionMetadata{}, err
+		}
+		resolvedVersion = resolveVersion(pkg, metadata)
 	}
-	var resolvedVersion string
+	if err := checkResolvedVersion(resolvedVersion); err != nil {
+		return types.VersionMetadata{}, fmt.Errorf("[METADATA] Failed to resolve version for package %s@%s", pkg.Name, pkg.Version)
+	}
+	slog.Info(fmt.Sprintf("[METADATA] Resolved package %s@%s with version %s", pkg.Name, pkg.Version, resolvedVersion))
+	return metadata.Versions[resolvedVersion], nil
+}
+
+func resolveVersion(pkg *types.Package, metadata *types.Metadata) string {
 	switch pkg.Version {
 	case "latest":
-		resolvedVersion = md.DistTags.Latest
+		return metadata.DistTags.Latest
 	case "next":
-		resolvedVersion = md.DistTags.Next
+		return metadata.DistTags.Next
+	case "":
+		return metadata.DistTags.Latest
+	case "*":
+		return metadata.DistTags.Latest
 	default:
-		resolvedVersion, err = utils.ResolveVersionForPackage(pkg, versionsList)
+		// handle edge case scenarios:
+		if strings.HasPrefix(pkg.Version, "http://") || strings.HasPrefix(pkg.Version, "https://") || strings.HasPrefix(pkg.Version, "git://") || strings.HasPrefix(pkg.Version, "git+") || strings.HasPrefix(pkg.Version, "npm:") {
+			slog.Error(fmt.Sprintf("[METADATA] URL Prefixes aren't being handled now. Failed to handle %s", pkg.Version))
+			return ""
+		}
+		if strings.ContainsRune(pkg.Version, '/') {
+			slog.Error(fmt.Sprintf("[METADATA] Github URLs aren't being handled now. Failed to handle %s", pkg.Version))
+			return ""
+		}
+		versions := make([]string, len(metadata.Versions))
+		i := 0
+		for k := range metadata.Versions {
+			versions[i] = k
+			i++
+		}
+		resolved, _ := utils.ResolveVersionForPackage(pkg, versions)
+		return resolved
 	}
-	if err != nil {
-		return types.VersionMetadata{}, fmt.Errorf("failed to resolve version for package %s@%s: %w", pkg.Name, pkg.Version, err)
-	}
+}
 
-	return md.Versions[resolvedVersion], nil
+func checkResolvedVersion(version string) error {
+	if version == "" {
+		return fmt.Errorf("failed to resolve version")
+	}
+	return nil
 }
 
 func GetListOfDependenciesFromVersionMetadata(md *types.VersionMetadata) []types.Package {
 	deps := make([]types.Package, len(md.Dependencies))
 	i := 0
-	for key, value := range (*md).Dependencies {
-		deps[i] = types.Package{Name: key, Version: value}
+	for name, version := range md.Dependencies {
+		deps[i] = types.Package{Name: name, Version: version}
 		i++
 	}
-
 	return deps
 }
